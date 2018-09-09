117a09ae8d6a61c74181a1112c3fe7e7ce37ce36
- <=>
2018-09-09 10:45:30 -0700

    Adds all 10 test files and tokens the files concurrently

--- src/main/java/project/group/cs157a/ProjectMain.java
+++ src/main/java/project/group/cs157a/ProjectMain.java
@@ -1,23 +1,32 @@
 package project.group.cs157a;
 
+import java.util.ArrayList;
+
 public class ProjectMain {
+	
+	private static final int NUMBER_OF_FILES = 10;
 
 	public static void main(String[] args) {
+		
+		
 //		DatabaseConnector dc = new DatabaseConnector();
 //		dc.createDatabase();
 //		dc.killConnection();
 //		
+		Tokenizer[] tokenizers = new Tokenizer[NUMBER_OF_FILES];
+		ArrayList<String[]> tokens = new ArrayList<>(10);
 		
-		Tokenizer tokenizer = new Tokenizer("Data(1).txt");
-		try {
-			String[] tokens = tokenizer.call();
-			for (String token: tokens) {
-				System.out.println(token);
+		for (int i = 1; i < NUMBER_OF_FILES; i++) {
+			tokenizers[i] = new Tokenizer("Data(" + i + ").txt");
+			try {
+				tokens.add(tokenizers[i].call());
+				System.out.println(tokens);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		}
+
 
 	}
 

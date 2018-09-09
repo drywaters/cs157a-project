3f8ffe3282da91198c6023c8bef66feb7666bb3c
- <=>
2018-09-09 11:20:50 -0700

    Fixed thread calls for tokenizer

--- src/main/java/project/group/cs157a/ProjectMain.java
+++ src/main/java/project/group/cs157a/ProjectMain.java
@@ -1,34 +1,53 @@
 package project.group.cs157a;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 public class ProjectMain {
-	
+
 	private static final int NUMBER_OF_FILES = 10;
 
 	public static void main(String[] args) {
-		
-		
+
+		// Commented out DB connection for testing concurrent token loading
 //		DatabaseConnector dc = new DatabaseConnector();
 //		dc.createDatabase();
 //		dc.killConnection();
 //		
-		
+
 		ExecutorService executor = Executors.newFixedThreadPool(10);
-		List<Future<String[]>> list = new ArrayList<>(10);
-		
-		Callable<String[]>[] tokenizer = new Tokenizer[10];
-		
-		for (int i = 1; i < NUMBER_OF_FILES; i++) {
-			Future<String[]> future = executor.submit(new Tokenizer("Data(" + i + ").txt"));
-			list.add(future);
+		List<String[]> tokensList = new ArrayList<>(10);
+		Callable<String[]>[] tokenizers = new Tokenizer[10];
+		List<Future<String[]>> futureValues = new ArrayList<>(10);
+
+		for (int i = 0; i < NUMBER_OF_FILES; i++) {
+			tokenizers[i] = new Tokenizer("Data(" + (i + 1) + ").txt");
+			futureValues.add(i, executor.submit(tokenizers[i]));
 		}
-		
+
+		// When thread finishes and returns a value, assign to ArrayList of tokens
+		// (String[])
+		for (Future<String[]> tokens : futureValues) {
+			try {
+				tokensList.add(tokens.get());
+			} catch (InterruptedException | ExecutionException e) {
+				e.printStackTrace();
+			}
+		}
+
+		// Received all threads, shutdown
+		executor.shutdown();
+
+		// Make sure all threads are there.
+		for (String[] tokens : tokensList) {
+			System.out.println(Arrays.toString(tokens));
+		}
 
 	}
 
